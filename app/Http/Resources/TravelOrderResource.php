0aa3e598df0432c83efabf2ba38df53228687be1
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-06-08 12:48:17 -0300

    Add Travel Order Resource

---
+++ app/Http/Resources/TravelOrderResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TravelOrderResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'order_id' => $this->order_id,
+            'destination' => $this->destination,
+            'start_date' => $this->start_date->format('Y-m-d'),
+            'end_date' => $this->end_date->format('Y-m-d'),
+            'status' => $this->status,
+            'cancel_reason' => $this->cancel_reason,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+                'email' => $this->user->email,
+            ],
+        ];
+    }
+}
